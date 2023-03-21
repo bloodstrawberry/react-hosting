f9230cee73e380d7ff56bb49307c80b970ed985e
bloodstrawberry <vvv3334@hanmail.net>
2023-03-21 22:27:36 +0900

    change build link
    change

--- src/page/Router2.js
+++ src/page/Router2.js
@@ -4,13 +4,13 @@ import { Unity, useUnityContext } from "react-unity-webgl";
 
 const Router2 = () => {
   const { unityProvider } = useUnityContext({
-    loaderUrl: "build/html-hosting.loader.js",
-    dataUrl: "build/html-hosting.data",
-    frameworkUrl: "build/html-hosting.framework.js",
-    codeUrl: "build/html-hosting.wasm",
+    loaderUrl: "react-hosting/build/html-hosting.loader.js",
+    dataUrl: "react-hosting/build/html-hosting.data",
+    frameworkUrl: "react-hosting/build/html-hosting.framework.js",
+    codeUrl: "react-hosting/build/html-hosting.wasm",
   });
 
-  return <Unity unityProvider={unityProvider} style={{width:"750px", height:"500px"}}/>;
+  return <Unity unityProvider={unityProvider} />;
 };
 
 export default Router2;
